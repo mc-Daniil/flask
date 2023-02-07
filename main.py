4107e0dde79123b8d7f5bc9a1dce982f34402c58
prifio <tolya_ash@mail.ru>
2023-02-07 17:01:57 +0300

    Task "Реклама с картинкой"

--- main.py
+++ main.py
@@ -39,5 +39,33 @@ def image_mars():
             """
 
 
+@app.route("/promotion_image")
+def promotion_image():
+    return f"""
+            <!doctype html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+                    
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                </head>
+                <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for("static", filename="img/mars.jpg")}" alt="Вот он - счастливчик на Марсе">
+                    <p class="first">Человечество вырастает из детства.</br></p>
+                    <p class="second">Человечеству мала одна планета.</br></p>
+                    <p class="third">Мы сделаем обитаемыми безжизненные пока планеты.</br></p>
+                    <p class="fourth">И начнем с Марса!</br></p>
+                    <p class="fifth">Присоединяйся!</br></p>
+                </body>
+            </html>
+            """
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
